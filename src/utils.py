3494249feac8b971ac243b95853a87718c1f4b25
osrokas <rokas.petravicius@idvilnius.lt>
2025-03-31 16:13:49 +0300

    add error handling

--- src/utils.py
+++ src/utils.py
@@ -15,15 +15,21 @@ def get_relative_altidute(img_path: str) -> float:
     ----------
     :return: Relative height of the image
     """
+    # Set default altitude to 0
+    altitude = 0
+
     # Open the image using PIL
     with Image.open(img_path) as img:
         # Extract the XMP metadata
-        xmp = img.getxmp()
+        try:
+            # Get the XMP metadata from the image
+            xmp = img.getxmp()
+            # Get relative height from the XMP metadata
+            altitude = xmp["xmpmeta"]["RDF"]["Description"]["RelativeAltitude"]
+            # Convert the altitude to a float
+            altitude = float(altitude)
+        except Exception as e:
+            print(f"Error extracting XMP metadata from image: {e}")
 
-    # Extract the relative altitude from the XMP metadata
-    altitude = xmp["xmpmeta"]["RDF"]["Description"]["RelativeAltitude"]
-
-    # Convert the altitude to a float
-    altitude = float(altitude)
-
+    # Extract the altitude value
     return altitude
