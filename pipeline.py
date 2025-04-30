ab2d3a3b82f3745a89ddc83133a2b7e9efb62eff
osrokas <rokas.petravicius@idvilnius.lt>
2025-04-30 10:57:46 +0300

    add arcgis insert

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,21 @@
 # Configs and utility
-import yaml 
 import os
+from datetime import datetime
 from tqdm import tqdm 
 import pandas as pd
+import uuid
+import requests
+import json
+from dotenv import load_dotenv
+
+# geospatial libraries
+import geopandas as gpd
+from shapely.geometry import Polygon, Point
 
 # Azure blobs
 from azure.storage.blob import BlobServiceClient
 
-# Deep learning library 
+# Deep learning library
 from ultralytics import YOLO 
 import cv2
 
@@ -20,33 +28,102 @@ from src.utils import (
     extract_focal_length
 )
 
-# Hardcoding the ID Vilnius drone camera parameters 
+# Hardcoding the ID Vilnius drone camera parameters
 SENSOR_WIDTH = 6.4
 SENSOR_HEIGHT = 4.8
+
+# Load environment variables
+if os.path.exists('.env'):
+    load_dotenv('.env', override=True, verbose=True)
+
+# Get the environment variables
+PORTAL_URL = os.getenv("PORTAL_URL")
+WEB_SERVICE_URL = os.getenv("WEB_SERVICE_URL")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+YOLO_MODEL = os.getenv("YOLO_MODEL")
+INPUT_CONNECTION_STRING = os.getenv("INPUT_CONNECTION_STRING")
+INPUT_CONTAINER_NAME = os.getenv("INPUT_CONTAINER_NAME")
+
+
+"""Define utils functions"""
+def authenticate(portal_url, username, password):
+    token_url = f"{portal_url}/sharing/rest/generateToken"
+    payload = {
+        "username": username,
+        "password": password,
+        "referer": "https://www.arcgis.com",
+        "f": "json",
+    }
+    response = requests.post(token_url, data=payload)
+    if response.status_code == 200:
+        token = response.json().get("token")
+        if token:
+            return token
+        else:
+            raise Exception(
+                "Failed to generate token: "
+                + response.json().get("error", {}).get("message", "Unknown error")
+            )
+    else:
+        response.raise_for_status()
+
+
+def insert_features_to_arcgis(url, features, token=None):
+    add_features_url = f"{url}/addFeatures"
+
+    payload = {
+        "features": json.dumps(features),
+        "f": "json",
+        "rollbackOnFailure": True,
+    }
+
+    if token:
+        payload["token"] = token
+
+    response = requests.post(add_features_url, data=payload)
+    return response.json()
+
+def query_features_from_arcgis(url, token, where_clause):
+    query_url = f"{url}/query"
+    params = {
+        "where": where_clause,
+        "outFields": "image_path",
+        "f": "json"}
+    if token:
+        params["token"] = token
+    response = requests.get(query_url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def wrangling_geometry(coords):
+    rings = []
+    for coord in coords:
+        ring = []
+        for point in coord:
+            point = list(point)
+            ring.append(point)
+        rings.append(ring)
+    return {"rings": rings}
+
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Reading the configuration file
-    with open("configuration.yaml", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    # Extracting the YOLO model name  
-    yolo_model_name = config["YOLO_MODEL"]
-    yolo_model_path = os.path.join("ml_models", yolo_model_name)
+    # Extracting the YOLO model name
+    yolo_model_path = os.path.join("ml_models", YOLO_MODEL)
     yolo_model = YOLO(yolo_model_path)
-
-    # Extracting the input connection string and container name
-    input_connection_string = config["INPUT_CONNECTION_STRING"]
-    input_container_name = config["INPUT_CONTAINER_NAME"]
 
     # Creating the connection
     blob_service_client = BlobServiceClient.from_connection_string(
-        input_connection_string
+        INPUT_CONNECTION_STRING
     )
 
     # Getting the container
-    container_client = blob_service_client.get_container_client(input_container_name)
+    container_client = blob_service_client.get_container_client(INPUT_CONTAINER_NAME)
 
     # Listing all the blobs
     blobs = container_client.list_blobs()
@@ -57,13 +134,28 @@ if __name__ == "__main__":
     # Dropping the blobs that are in the 00_UNSORTED directory
     blobs = [blob for blob in blobs if "00_UNSORTED" not in blob.name]
 
-    # Creating the input directory to store the downloaded images 
+    token = authenticate(PORTAL_URL, USERNAME, PASSWORD)
+    # Querying the ArcGIS for the blobs that are already there
+    arcgis_blobs = query_features_from_arcgis(
+        url=WEB_SERVICE_URL, token=token, where_clause="1=1"
+    )
+
+    # Extracting the features from the ArcGIS response
+    arcgis_blobs = arcgis_blobs.get("features", [])
+
+    # Filtering out the blobs that are already in ArcGIS
+    if arcgis_blobs:
+        arcgis_blob_names = [feature["attributes"]["image_path"] for feature in arcgis_blobs]
+        blobs = [blob for blob in blobs if blob.name not in arcgis_blob_names]
+
+    # Creating the input directory to store the downloaded images
     input_dir = os.path.join(current_dir, "blob_input")
     os.makedirs(input_dir, exist_ok=True)
 
     # Placeholder for the parsed information
-    sosnovskies = []
+    sosnovskies, probabilities, classes = [] , [], []
 
+    # Iterating over the blobs
     for blob in tqdm(blobs):
         # Getting the base name
         base_name = os.path.basename(blob.name)
@@ -79,18 +171,22 @@ if __name__ == "__main__":
 
         # Downloading the blob
         blob_client = blob_service_client.get_blob_client(
-            container=input_container_name, blob=blob.name
+            container=INPUT_CONTAINER_NAME, blob=blob.name
         )
         with open(path, "wb") as f:
             f.write(blob_client.download_blob().readall())
 
-        # Reading and ploting the image 
+        # Reading and ploting the image
         img = cv2.imread(path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # Predicting 
+        # Predicting
         results = yolo_model.predict(img, conf=0.05)
         segments = results[0].masks
+
+        # Get probabilities and classes
+        probs = results[0].boxes.conf
+        mask_classes = results[0].boxes.cls
 
         if segments is not None: 
             # Gathering all the needed information for the Sosnovskies
@@ -109,15 +205,15 @@ if __name__ == "__main__":
             if gsd_h is None or gsd_v is None:
                 print(f"Skipping image {base_name} due to invalid GSD values.")
                 continue
-            
+
             # Creating the gps coordinates
             polygon_idx = 0
-            for mask in segments:
+            for mask, prob, mask_cls in zip(segments, probs, mask_classes):
                 points = mask.xy[0]
                 for point in points:
                     sosnovskies.append(
                         (
-                            f"{blob_dir}/{base_name}",
+                            f"{blob.name}",
                             polygon_idx,
                             pixel_to_gps(
                                 point[0], 
@@ -135,30 +231,104 @@ if __name__ == "__main__":
                 # Adding an additional last point as the first point
                 sosnovskies.append(
                     (
-                        f"{blob_dir}/{base_name}",
+                        f"{blob.name}",
                         polygon_idx,
                         pixel_to_gps(
-                            points[0][0], 
-                            points[0][1], 
-                            img.shape[1], 
-                            img.shape[0], 
-                            latitude, 
-                            longitude, 
-                            gsd_h, 
-                            gsd_v, 
-                            relative_altitude
-                        )
+                            points[0][0],
+                            points[0][1],
+                            img.shape[1],
+                            img.shape[0],
+                            latitude,
+                            longitude,
+                            gsd_h,
+                            gsd_v,
+                            relative_altitude,
+                        ),
                     )
                 )
 
                 # Incrementing
                 polygon_idx += 1
 
+                probabilities.append(round(float(prob), 2))
+                classes.append(int(mask_cls))
+
         # Clearning the image
         del img 
         os.remove(path)
 
+    # Checking if there are any Sosnovskies found
+    # if not finish pipeline
+    if not sosnovskies:
+        print("No Sosnovskies found in the images.")
+        exit(0)
+
+    # Create dataframe from the Sosnovskies list
     gps_points_df = pd.DataFrame(sosnovskies, columns=["image_path", "polygon_idx", "gps_coords"])
     gps_points_df['lat'] = gps_points_df['gps_coords'].apply(lambda x: x[0])
     gps_points_df['lon'] = gps_points_df['gps_coords'].apply(lambda x: x[1])
-    gps_points_df.drop(columns=['gps_coords'], inplace=True)
+
+    # Dropping the gps_coords column
+    gps_points_df.drop(columns=["gps_coords"], inplace=True)
+
+    # Grouping the data by image path and polygon index
+    gdf = gps_points_df.groupby(['image_path', 'polygon_idx']).agg(list).reset_index()
+
+    # Assigning probabilities and classes to the dataframe
+    gdf["prediction_prob"] = probabilities
+    gdf['species'] = classes
+
+    gdf["begin_lifespan_version"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # gdf["begin_lifespan_version"] = gdf.apply(
+    #     lambda x: pd.Timestamp(x["begin_lifespan_version"]).timestamp(), axis=1
+    # )
+
+    # Creating the geometry column
+    gdf["geometry"] = gdf.apply(
+        lambda row: Polygon([Point(xy) for xy in zip(row["lon"], row["lat"])]),
+        axis=1,
+    )
+
+    # Creating the GeoDataFrame
+    gdf = gpd.GeoDataFrame(gdf, geometry="geometry", crs="EPSG:4326")
+    gdf = gdf.to_crs("EPSG:3346")
+    gdf["population_size"] = round(gdf.geometry.area, 2)
+    gdf['created_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    gdf['updated_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Create a unique ID for each prediction UUID
+    gdf['prediction_id'] = gdf.apply(lambda x: str(uuid.uuid4()), axis=1)
+
+    # Drop unnecessary columns
+    gdf = gdf.drop(columns=["lat", "lon", "polygon_idx"])
+
+    # Drop null polygon geometries
+    gdf = gdf[gdf["population_size"] > 0.00]
+    gdf.reset_index(drop=True, inplace=True)
+
+    # Transform the GeoDataFrame to a dictionary format suitable for ArcGIS
+    geojson_data = gdf.to_geo_dict(drop_id=True)
+
+    # Get features from the GeoJSON data
+    features = geojson_data.get("features", [])
+
+    # Update features with the correct geometry format for ArcGIS
+    agol_features = []
+    for feature in features:
+        # Get coordinates from the feature
+        coords = feature["geometry"]["coordinates"]
+        # Create geometry in ArcGIS format
+        rings = wrangling_geometry(coords)
+        # Create the ArcGIS feature
+        agol_feature = {
+            "geometry": rings,
+            "attributes": feature["properties"],
+        }
+        # Append the feature to the list
+        agol_features.append(agol_feature)
+
+    # Insert features into ArcGIS
+    token = authenticate(PORTAL_URL, USERNAME, PASSWORD)
+    insert_features_to_arcgis(WEB_SERVICE_URL, agol_features, token)
+    # insert_features_to_arcgis(WEB_SERVICE_URL,  agol_features)
